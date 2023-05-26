4a7f0d771f804c70971cca2bf85f164788a1c214
Bullwhale <99353977+Le-Bullwhale@users.noreply.github.com>
2023-05-26 22:00:13 +0930

    Update isking.sh

--- GameOver/isking.sh
+++ GameOver/isking.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-file_path="./root/king.txt"
+file_path=$(find / -name "king.txt" 2>/dev/null)
 keyword="Bsokimi"
 
-# Read the first line of the file
-first_line=$(head -n 1 "$file_path")
+# Check if the file exists
+if [ -f "$file_path" ]; then
+  # Read the first line of the file
+  first_line=$(head -n 1 "$file_path")
 
-# Check if the first line contains the keyword
-if [[ "$first_line" == *"$keyword"* ]]; then
-  message="Attention All! The king is now '$keyword'. All hail '$keyword'!"
-  echo "$message" | wall
+  # Check if the first line contains the keyword
+  if [[ "$first_line" == *"$keyword"* ]]; then
+    message="Attention All! The king is now '$keyword'. All hail '$keyword'!"
+    echo "$message" | wall
+  fi
 fi
